ea27b185d7b944f3f0c638352856ed78c2dd221c
Yuki Okushi <jtitor@2k36.org>
2022-05-17 01:09:08 +0900

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-const siteTitle = "My blog";
+const siteTitle = "echelon";
 
 export default function Layout({ children }) {
   return (
